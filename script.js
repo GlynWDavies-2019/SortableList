d6930c75ca3727a97e02a8de4436ef51565ac109
Glyn Davies <glynwdavies@gmail.com>
2020-06-16 20:12:20 +0100

    Create list functionality implemented

---
+++ script.js
@@ -0,0 +1,41 @@
+const draggableList = document.getElementById('draggable-list');
+const checkButton = document.getElementById('check');
+
+const richestPeople = [
+    "Jeff Bezos",
+    "Bill Gates",
+    "Warren Buffett",
+    "Bernard Arnault",
+    "Carlos Slim Helu",
+    "Amancio Ortega",
+    "Larry Ellison",
+    "Mark Zuckerberg",
+    "Michael Bloomberg",
+    "Larry Page"
+];
+
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Spread operator [...] makes a copy of an array
+// data-index is a custom attribute
+
+function createList() {
+  [...richestPeople]  
+    .forEach((person,index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-index',index);
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>
+        `;
+        listItems.push(listItem);
+        draggableList.appendChild(listItem);
+    });
+}
